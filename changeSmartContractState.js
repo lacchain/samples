4015e247f408db44a1cd4900ad4d13122d7428ff
ccamaleon5 <adriancc5.5@gmail.com>
2021-12-13 15:41:24 -0500

    add erc20 bytecode

--- changeSmartContractState.js
+++ changeSmartContractState.js
@@ -5,7 +5,7 @@ const sha3 = require("js-sha3").keccak_256
 
 let set
 
-const setRecipientContract = () => { //Set for RelayHub Contract
+/*const setRecipientContract = () => { //Set for RelayHub Contract
   console.log("#######################Preparing value for Recipient SmartContract #######################")
   const functionName = "store" //store(uint256)
   const typeOfData = "uint256"
@@ -24,7 +24,31 @@ const setRecipientContract = () => { //Set for RelayHub Contract
   const txData = set + value.substr(2)
   console.log("txData:"+txData)
   return txData
+}*/
+
+const setRecipientContract = () => { //Set for RelayHub Contract
+  console.log("#######################Preparing value for Recipient SmartContract #######################")
+  const functionName = "transfer" //store(uint256)
+  const recipientData = "address"	
+  const typeOfData = "uint256"
+
+  // store(uint256)
+
+  const recipient = "0x173CF75f0905338597fcd38F5cE13E6840b230e9"	
+  const number = 1
+  const nodeAddress = "0xd00e6624a73f88b39f82ab34e8bf2b4d226fd768"
+  const expiration = 1636394529
+
+  let set = web3.eth.abi.encodeFunctionSignature(`${functionName}(${recipientData},${typeOfData})`)//function name to use
+
+  let value = web3.eth.abi.encodeParameters(
+    ["address","uint256","address","uint256"],
+    [recipient,number,nodeAddress,expiration])//setting the value
+  const txData = set + value.substr(2)
+  console.log("txData:"+txData)
+  return txData
 }
+
 
 const chooseSmartContractSetter = () => {
     set = setRecipientContract
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const {buildTransaction} = require('./pantheon_utils/web3Operations')
 const {sendTransactionAndProcessIncommingTx} = require("./lib/helpers")
 const {set} = require('./changeSmartContractState')
 let txData = set()  
-const addressFrom = "0x173CF75f0905338597fcd38F5cE13E6840b230e9"
+//const addressFrom = "0x173CF75f0905338597fcd38F5cE13E6840b230e9"
+const addressFrom = "0xbcEda2Ba9aF65c18C7992849C312d1Db77cF008E"
 
 const getData = async(addressFrom) => {
     try{
@@ -11,10 +12,10 @@ const getData = async(addressFrom) => {
         console.log(txCount);	
         const valueInEther = "0"
         const gasLimit = "200000"
-        const addressTo = "0xeA23c1683F3e8F9e13A4AFF8037736513F4Ae59b"
+        const addressTo = "0x6c8ac1D53B99936d1478528906D9B2D1DF1Db973"
         //const privKey = "b3e7374dca5ca90c3899dbb2c978051437fb15534c945bf59df16d6c80be27c0"
-        const privKey = "0E273FFD9CF5214F7D6ADE5D1ABFD6D101B648AF12BC2DE6AC4AFCB4DB805CD3"
-        //const privKey = "919b7e0e4095ce8a2cb22cea25a4d5888981d29d03cbdc714ed4b5f58313fdc6"
+        //const privKey = "0E273FFD9CF5214F7D6ADE5D1ABFD6D101B648AF12BC2DE6AC4AFCB4DB805CD3"
+        const privKey = "919b7e0e4095ce8a2cb22cea25a4d5888981d29d03cbdc714ed4b5f58313fdc6"
         const txObject = buildTransaction(txCount,gasLimit,addressTo,valueInEther,txData)
         sendTransactionAndProcessIncommingTx(txObject,privKey)
     }catch(e){
